cc6f0643d8041cca240eb94bb43ddd73ef5befa2
Yutong Zhang <yutongz@google.com>
2018-12-02 17:59:42 -0800

    Create MyDBClient.java

---
+++ MyDBClient.java
@@ -0,0 +1,26 @@
+Class MyDBClinet{
+
+    Connection conn;
+  
+    MyDBClient(){
+      //Prepare RDS authentication
+    
+       //Create Connect
+       conn = xxx;
+    }
+    
+    Boolean CreateTable(String tableName){
+        //Create a Table
+    }
+    
+    Boolean QueryDriverInfo(int driverID) {
+        ...
+    }
+    
+    Boolean QueryTrips() {
+    
+    }
+    
+    
+
+}
